29d6e348c63ba944b0bfaf3a3ccaf260cbbdf515
Justin Toth <JustinToth@ufl.edu>
2025-03-12 19:55:36 -0400

    updated router to include new routes

--- Misc/IN_PROGRESS_WORK/new/UFID_Reader_webapp/fastapi_backend/app/api/router.py
+++ Misc/IN_PROGRESS_WORK/new/UFID_Reader_webapp/fastapi_backend/app/api/router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import students, users
+from app.api.routes import users, students, kiosks, exams
 
 api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(students.router)
+api_router.include_router(kiosks.router)
+api_router.include_router(exams.router)
 
-#api_router.include_router(exams.router)
-#api_router.include_router(kiosks.router)
 #api_router.include_router(login.router)
 #api_router.include_router(utils.router)

c92ea6e02579503741fb315b620df09c2100dbe5
Justin Toth <JustinToth@ufl.edu>
2025-03-12 20:44:46 -0400

    included course routes in router

--- Misc/IN_PROGRESS_WORK/new/UFID_Reader_webapp/fastapi_backend/app/api/router.py
+++ Misc/IN_PROGRESS_WORK/new/UFID_Reader_webapp/fastapi_backend/app/api/router.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, students, kiosks, exams
+from app.api.routes import users, students, kiosks, exams, courses
 
 api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(students.router)
 api_router.include_router(kiosks.router)
 api_router.include_router(exams.router)
+api_router.include_router(courses.router)
 
 #api_router.include_router(login.router)
 #api_router.include_router(utils.router)

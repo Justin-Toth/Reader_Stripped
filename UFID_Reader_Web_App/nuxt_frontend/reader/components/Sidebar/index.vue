285f9d2ca949260476fe4de6caf74814ed01949d
strejo9811 <strejo@ufl.edu>
2025-03-27 20:05:00 -0400

    Nav improvments

--- UFID_Reader_Web_App/nuxt_frontend/reader/components/Sidebar/index.vue
+++ UFID_Reader_Web_App/nuxt_frontend/reader/components/Sidebar/index.vue
@@ -7,7 +7,7 @@ import {
 
 <template>
     <div class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-        <aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <aside class="fixed inset-y-0 left-0 z-10 hidden w-[70px] flex-col border-r bg-background sm:flex">     //Increased width bc added labels and too large
             <nav class="flex flex-col items-center gap-4 px-2 py-4">
 
                 <!--Company Logo-->

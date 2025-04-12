0acfcc676fba593318c975cde3c5355bf22980a2
Justin-Toth <justintoth@ufl.edu>
2025-04-12 17:06:35 -0400

    Update

--- UFID_Reader/Models/AuthResult.cs
+++ UFID_Reader/Models/AuthResult.cs
@@ -3,7 +3,7 @@
 public class AuthResult
 {
     public bool IsSuccess { get; set; }
-    public string Message { get; set; }
+    public required string Message { get; set; }
 
     public static AuthResult Success() => new AuthResult { IsSuccess = true, Message = "Authentication successful." };
     public static AuthResult Failure(string reason) => new AuthResult { IsSuccess = false, Message = reason };

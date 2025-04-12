d55a50df16ac0f2115a2801261ea8514423bf03e
Justin-Toth <justintoth@ufl.edu>
2025-04-12 16:31:34 -0400

    reorganized default layout

--- UFID_Reader_Web_App/nuxt_frontend/reader/layouts/default.vue
+++ UFID_Reader_Web_App/nuxt_frontend/reader/layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex min-h-screen w-full flex-col bg-muted/40">
-        <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <!-- Sidebar-->
+        <Sidebar />
+
+        <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-[150px]">
             <!--Header-->
             <Header />
-
-            <!-- Sidebar-->
-            <Sidebar />
 
             <!--Page Content-->
             <slot />

6e932201cf6025361fb2111594c10ea97b9930bb
Justin-Toth <justintoth@ufl.edu>
2025-04-12 16:29:26 -0400

    updated logo to include text

--- UFID_Reader_Web_App/nuxt_frontend/reader/components/CompanyLogo.vue
+++ UFID_Reader_Web_App/nuxt_frontend/reader/components/CompanyLogo.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="group flex h-12 w-12 items-center justify-center rounded-full md:h-8 md:w-8 md:text-base">
-        <img src="@/assets/gators_logo.png" alt="logo" >
+    <div class="flex items-center gap-3">
+        <img src="@/assets/gators_logo.png" alt="logo" class="h-8 w-auto" >
+        <span class="font-semibold text-lg hidden sm:inline">UFID</span>
     </div>
-</template>
+  </template>
+  

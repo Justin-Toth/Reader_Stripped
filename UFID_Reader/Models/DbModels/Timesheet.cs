736d4212391abb0a85074c0f197137ceb685ff54
Justin Toth <JustinToth@ufl.edu>
2025-04-16 02:45:48 -0400

    created timesheet model

---
+++ UFID_Reader/Models/DbModels/Timesheet.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+
+namespace UFID_Reader.Models.DbModels;
+
+public class Timesheet
+{
+    public string Ufid{ get; set; } 
+    public string Full_name { get; set; }
+    public string Course_code { get; set; }
+    public string Section_num { get; set; }
+    public string Date { get; set; }
+    public string Swipe_time { get; set; }
+}

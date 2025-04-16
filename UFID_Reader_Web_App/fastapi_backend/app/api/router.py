28f659b3605002d46e9086b51d4d8dc25492d9d9
Justin Toth <JustinToth@ufl.edu>
2025-04-16 02:43:22 -0400

    add timesheet routes to router

--- UFID_Reader_Web_App/fastapi_backend/app/api/router.py
+++ UFID_Reader_Web_App/fastapi_backend/app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, students, kiosks, exams, courses
+from app.api.routes import users, students, kiosks, exams, courses, timesheet
 
 api_router = APIRouter()
 api_router.include_router(users.router)
@@ -8,6 +8,7 @@ api_router.include_router(students.router)
 api_router.include_router(kiosks.router)
 api_router.include_router(exams.router)
 api_router.include_router(courses.router)
+api_router.include_router(timesheet.router)
 
 #api_router.include_router(login.router)
 #api_router.include_router(utils.router)
